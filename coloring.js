006e34e38dfafdfd31eaf6167c218556288e9640
Neil Soiffer <soiffer@alum.mit.edu>
2020-01-08 23:07:24 -0800

    More improvements and bug fixes. "change" and "cancel" buttons are now hooked up.

--- coloring.js
+++ coloring.js
@@ -58,7 +58,6 @@ class ColoringRules {
     constructor() {
         /** @type {ColorRule[]} */
         this.patterns = [];
-        this.initialize();
     }
 
     /**
@@ -70,6 +69,9 @@ class ColoringRules {
         return newRules;
     }
 
+    /**
+     * @returns {ColoringRules}
+     */
     initialize() {
         this.patterns.push( new ColorRule('3', 'hsl(130, 70%, 43%)', 'hsl(4, 90%, 50%)', 'normal', '') );
         this.patterns.push( new ColorRule('8', 'hsl(4, 90%, 50%)', 'hsl(130, 70%, 43%)', 'normal', '') );
@@ -79,6 +81,7 @@ class ColoringRules {
         this.patterns.push( new ColorRule('-', '', '', 'bold', '.222em') );
         this.patterns.push( new ColorRule('\\|', '', '', 'bold', '') );
         this.patterns.push( new ColorRule('<|=|>|≠|≤|≥', 'hsl(0,0%,100%)', 'hsl(160, 10%, 10%)', 'normal', '.278em') );
+        return this;
     }
 
     /**
@@ -112,6 +115,20 @@ class ColoringRules {
         }
     }
 
+    /**
+     * Adds the new rules to the old ones (this) -- new ones first (or replacing old ones)
+     * A clone of 'this' is made ('this' is not changed)
+     * @param {ColoringRules} newRules  (not changed)
+     * @returns {ColoringRules} 
+     */
+    merge(newRules) {
+        let clone = this.clone();
+        for (const rule of newRules.patterns) {
+            clone.replace(rule.pattern.source, rule);
+        }
+        return clone;
+    }
+
     /** 
      * @param {string} str // should be a single character
      * @returns {string}
@@ -134,29 +151,48 @@ class ColoringRules {
             }
         }
     }
+
+    updateAll() {
+        this.updatePalettes();
+
+        const testArea = document.getElementById('test-input');
+        testArea.innerHTML = this.convertToSpan(testArea.innerText);
+    }
 }
 
 // The global (permantent) instance of coloring rules
-ColoringRules.Rules = new ColoringRules();
+ColoringRules.Rules = new ColoringRules().initialize();
 
 
 
 const EditAreaIds = ['edit-input', 'text-color', 'bg-color', 'font-style', 'spacing'];
-const PaletteIds = ['lc-letters', 'uc-letters', 'digits', 'symbols']
+const PaletteIds = ['lc-letters', 'uc-letters', 'digits', 'symbols', 'symbols-test']
 window.onload =
     function() {
         // add the palettes
         PaletteIds.forEach( palette => addCharacterPalette(document.getElementById(palette)) );
 
-        let inputArea = document.getElementById('color-input');
-        inputArea.addEventListener('keydown', updateTestArea);
-        document.getElementById('edit-input').addEventListener('keydown', updateTestArea);
+        let testInput = document.getElementById('test-input');
+        testInput.addEventListener('keydown', updateTestArea);
+
+        let editInputArea = document.getElementById('edit-input');
+        editInputArea.addEventListener('keydown', updateTestArea);
 
         EditAreaIds.forEach(
-            id => document.getElementById(id).addEventListener('input', updateCharStyle));       
+            id => document.getElementById(id).addEventListener('input', updateCharStyle));
+            
+            document.getElementById('change').addEventListener('mousedown', function() {
+                ColoringRules.Rules = ColoringRules.Rules.merge(gatherCharStyle(editInputArea));
+                ColoringRules.Rules.updateAll();
+                editInputArea.innerText = '';   // clear the work area input
+            });
+            document.getElementById('cancel').addEventListener('mousedown', function() {
+                ColoringRules.Rules.updateAll();
+                editInputArea.innerText = '';   // clear the work area input
+            });
 
-        // useful for demo at the moment
-        inputArea.innerHTML = ColoringRules.Rules.convertToSpan(inputArea.textContent);
+        // useful for demo at the moment to have some initial contents
+        testInput.innerHTML = ColoringRules.Rules.convertToSpan(testInput.textContent);
     };
 
 /**
@@ -170,11 +206,16 @@ function addCharacterPalette(grid) {
         let button = document.createElement('button');
         button.className = 'button';
         button.innerHTML = ColoringRules.Rules.convertToSpan(char);
-        button.onclick = copyCharToEditArea;
+        button.onclick = /test/.test(grid.id) ? copyCharToTestArea : copyCharToEditArea;
         grid.appendChild(button);
     } );
 }
 
+/**
+ * Convert a string of the form rgb(dec, dec, dec) to #rrggbb
+ * @param {string} rgbColor 
+ * @return {string}
+ */
 function toNumericColor(rgbColor) {
     const numbers = rgbColor.match(/rgb\((\d+), (\d+), (\d+)\)/).slice(1);
     const asHex =  numbers.map( num => parseInt(num).toString(16));
@@ -186,12 +227,27 @@ function toNumericColor(rgbColor) {
  * 
  * @param {MouseEvent} ev 
  */
+function copyCharToTestArea(ev) {
+    ev.preventDefault();
+    document.execCommand('insertHTML', false, ColoringRules.Rules.convertToSpan(ev.target.innerText));
+}
+
+/**
+ * 
+ * @param {MouseEvent} ev 
+ */
 function copyCharToEditArea(ev) {
-    let target = document.getElementById('edit-input');
-    target.innerHTML = ColoringRules.Rules.convertToSpan(this.innerText);
+    let editInputArea = document.getElementById('edit-input');
+    if (editInputArea.innerText) {
+        if (confirm('Your changes to the character have not been saved.\nClick "ok" to save the changes and "cancel" to discard the changes')) {
+            ColoringRules.Rules = ColoringRules.Rules.merge(gatherCharStyle(editInputArea));
+        }
+        ColoringRules.Rules.updateAll();
+    }
+    editInputArea.innerHTML = ColoringRules.Rules.convertToSpan(this.innerText);
 
     // update the edit area fields
-    let targetStyle = target.children[0].style;
+    let targetStyle = editInputArea.children[0].style;
     document.getElementById('text-color').value = targetStyle.color ? toNumericColor(targetStyle.color) : '#000000';
     document.getElementById('bg-color').value = targetStyle.backgroundColor ? toNumericColor(targetStyle.backgroundColor) : '#ffffff';
     document.getElementById('font-style').value = targetStyle.fontStyle ? targetStyle.fontStyle : 
@@ -210,7 +266,32 @@ function updateTestArea(e) {
     if (e.key.length === 1) {
         e.preventDefault();
         document.execCommand('insertHTML', false, ColoringRules.Rules.convertToSpan(e.key));
+        if (/<br/.test(document.getElementById('test-input').innerHTML)) {
+            // for reasons I haven't been able to figure out, sometimes <br> gets inserted also (not by this code)
+            // at least it does in Chrome (not in Firefox AFAIK)
+            // this pulls it back out
+            document.execCommand('forwardDelete', false, null);
+        }
     }
+}
+
+/**
+ * @param {HTMLElement} elementForChar
+ * @returns {ColoringRules} 
+ */
+function gatherCharStyle(elementForChar) {
+    // @ts-ignore '.value' is issue
+    const ruleValues = EditAreaIds.slice(1).map(  id => document.getElementById(id).value);
+
+    // create a rule based on those values and *add* it to a temporary set of rules
+    let newRules = new ColoringRules();
+
+    for (const ch of elementForChar.innerText.split('')) {      // for loop because there could be more than one char
+        // @ts-ignore
+        const newRule = new ColorRule( ...[newRules.escapeSpecialChars(ch)].concat(ruleValues) );    
+        newRules.replace(ch, newRule);
+    }
+    return newRules;
 }
 
 /**
@@ -219,19 +300,8 @@ function updateTestArea(e) {
  */
 function updateCharStyle(e) {
     // gather up all the values that were set
-    let elementForChar = document.getElementById('edit-input');     // not an input field, so can't use value
-    // @ts-ignore '.value' is issue
-    const ruleValues = EditAreaIds.slice(1).map(  id => document.getElementById(id).value);
-
-    // create a rule based on those values and *add* it to a temporary set of rules
-    let tempColoringRules = ColoringRules.Rules.clone();
-
-    for (const ch of elementForChar.innerText.split('')) {      // for loop because there could be more than one char
-        // @ts-ignore
-        const newRule = new ColorRule( ...[ColoringRules.Rules.escapeSpecialChars(ch)].concat(ruleValues) );    
-        tempColoringRules.replace(ch, newRule);
-    }
-
+    const elementForChar = document.getElementById('edit-input')
+    const tempColoringRules = ColoringRules.Rules.merge(gatherCharStyle(elementForChar));
     // @ts-ignore
     if (typeof e.inputType !== 'undefined') {
         // typing directly in the field
@@ -242,9 +312,5 @@ function updateCharStyle(e) {
         elementForChar.innerHTML = tempColoringRules.convertToSpan(elementForChar.innerText);
     }
 
-
-    tempColoringRules.updatePalettes();
-    
-    const testArea = document.getElementById('color-input');
-    testArea.innerHTML = tempColoringRules.convertToSpan(testArea.innerText);
+    tempColoringRules.updateAll();
 }
